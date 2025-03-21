b368540deeccd3d39d1989113a62d1da44ee76da
Sudharshana B <sudhubalu04@gmail.com>
2025-03-21 12:09:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u sudharshanab -p dckr_pat_PkDnMdDslEqeqx333B0bA-h95Dw
-    docker tag test1 sudharshanab/dev
-    docker push sudharshanab/dev
+    docker tag test1 sudharshanab/docker-image
+    docker push sudharshanab/docker-image
    
     
