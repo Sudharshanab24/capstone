bf12d9d44ef95e2bdffde24a254654b4145590b2
Sudharshana B <sudhubalu04@gmail.com>
2025-03-21 11:27:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u sudharshanab -p dckr_pat_PkDnMdDslEqeqx333B0bA-h95Dw
+    docker tag test1 sudharshanab/dev
+    docker push sudharshanab/dev
+   
     
